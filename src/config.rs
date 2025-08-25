3ec01f9c10fbfc61a777970407913e4e67b74215
fufesou <linlong1266@gmail.com>
2025-08-26 00:04:04 +0800

    feat: advanced option, main window, always on top
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -2509,6 +2509,7 @@ pub mod keys {
     pub const OPTION_ALLOW_HTTPS_21114: &str = "allow-https-21114";
     pub const OPTION_ALLOW_HOSTNAME_AS_ID: &str = "allow-hostname-as-id";
     pub const OPTION_HIDE_POWERED_BY_ME: &str = "hide-powered-by-me";
+    pub const OPTION_MAIN_WINDOW_ALWAYS_ON_TOP: &str = "main-window-always-on-top";
 
     // flutter local options
     pub const OPTION_FLUTTER_REMOTE_MENUBAR_STATE: &str = "remoteMenubarState";
@@ -2681,6 +2682,7 @@ pub mod keys {
         OPTION_ALLOW_HOSTNAME_AS_ID,
         OPTION_REGISTER_DEVICE,
         OPTION_HIDE_POWERED_BY_ME,
+        OPTION_MAIN_WINDOW_ALWAYS_ON_TOP,
     ];
 }
 
