c406111c668a8f50f339a319ff7bb7acdcd56bd1
lc <lichon@foxmail.com>
2025-11-13 23:22:41 +0800

    make compiler happy

--- src/webrtc_dummy.rs
+++ src/webrtc_dummy.rs
@@ -13,7 +13,31 @@ pub struct WebRTCStream {
     // mock struct
 }
 
+impl Clone for WebRTCStream {
+    fn clone(&self) -> Self {
+        WebRTCStream {
+        }
+    }
+}
+
 impl WebRTCStream {
+
+    pub async fn new(
+        _: &str,
+        _: u64,
+    ) -> ResultType<Self> {
+        Ok(Self {})
+    }
+
+    #[inline]
+    pub async fn get_local_endpoint(&self) -> ResultType<String> {
+        Ok(String::new())
+    }
+
+    #[inline]
+    pub async fn set_remote_endpoint(&self, _: &str) -> ResultType<()> {
+        Ok(())
+    }
 
     #[inline]
     pub fn set_raw(&mut self) {
